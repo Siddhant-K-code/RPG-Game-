c7afee8588d40e3fc22722433f56c1628762f022
Siddhant Khare <Siddhantkhare2694@gmail.com>
2020-03-01 15:57:09 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,107 @@
+import time 
+print("Wait...")
+time.sleep(5)
+print("Pro tip: Once you press enter you can't go back hope to fix that in the future. Now to the game.")
+time.sleep(3)
+print("Loading...")
+time.sleep(2.5)
+gender = input("What is your gender?")
+name =  input("What is your nickname?")
+print(name)
+print("I think that'll do.")
+print ("Okay agent. I am Sally. These are some high tech glasses that let you see things that other people can't see. Heres the story. We are a secret society and... ")
+time.sleep(2.111111111)
+print("A bell starts ringing")
+print("Sorry agent we have duty. She says")
+print("")
+A = print("A: Search for a way out ")
+B = print("B: Run around flailing and screaming")
+C = print("C: Analyze the situation")
+user = input("What do you do?") 
+if user == ("A"):
+  print("You find a door")
+if user == ("B"):
+  print("Nobody helps you. It was a waste of time")
+if user == ("C"):
+  print("It seems that this doesn't happen much, a lot of agents look puzzled. When you are looking around you found a door.")
+if user == ("B"):
+  print("When you were flailing and screaming you caught a glimpse of a door.")
+print("A: Open the door")
+print("B: Ignore and look for more things")
+user1 = input("What do you do?")
+if user1 == ("B"):
+  print("You found a atom blaster quality: Epic")
+if user1 == ("A"):
+  print("The door is locked, and it looks very thick.")
+if user1 == ("B"):
+  print("Your gut tells you to go back to the door. You try to get out but it doesn't work. ")
+if user1 == ("A"):
+  print("Your gut tells you to go scan the room again. You go back into the room and look around. You find a blaster: quality Epic. You go back to the door")
+print("")
+user2 = input("Would you like to blast it open?")
+if user2 == ("Yes"):
+  print("You blast it open")
+if user2 == ("No"):
+  print("YOU HAVE TO!!!! You blast it open")
+print("Every body escapes from the door.")
+print("When you get outside you see that alien- like-things that are shooting at the head quarters")
+print("A: Find cover and shoot back at them ")
+print("B:Hide ")
+user3 = input("What do you do?")
+if user3 == ("A"):
+  print("You find a block of brick that has fallen off of headquarters. It is big enough for you to have some cover.")
+if user3 == ("B"):
+  print("You find a block of brick that has fallen off headquarters. You hide behind it.")
+print("Behind the rock you find a large frying pan. You pick it up. ")
+print("A: Take cover with the frying pan and loot the fallen aliens. ")
+print("B: Stay hiding")
+user4 = input("What do you do?")
+if user4 == ("A"):
+  print("You take cover with the frying pan. The aliens are shooting at you. You dodge left to right. You get to the nearest fallen alien. You pick up his gun it is: a SCAR quality: rare. Suddenly the wall behind your cover began to crumble from a tank blast. Luckily you weren't there anymore.")
+if user4 == ("B"):
+  print("You stay there. Suddenly an alien tank blast shoots through the air. It hits a wall behind you. It comes tumbling down. You run with the frying pan up. You are near one of the fallen aliens. It looks like it has a SCAR. You pick it up. SCAR quality: rare.")
+  time.sleep(8)
+print("")
+print("You run back to where all the other agents are hiding for cover.")
+print("You are where all the other agents are.")
+time.sleep(2.222)
+print("All of sudden more agents are there.")
+print("Then all of a sudden an alien throws a grenade. It is distant but it still sends you flying.")
+time.sleep(3.777777777)
+print("")
+print("You end up in some other part of the city. You think other humans would notice but it seems that they are having a perfectly normal day.")
+print("You still have all your stuff.")
+print("You put your high tech glasses on")
+time.sleep(0.99999)
+print("You see everything. The alien spaceships, the headquarters. Everything.")
+print("A: Go explore the streets to see if you can find more stuff.")
+print("B: Go find some healng things. You think you broke your leg.")
+user5 = input("What do you do?")
+if user5 == ("A"):
+  print("You think you broke your leg. You find a medical kit from a nice lady at the pantry.")
+if user5 == ("B"):
+  print("Somebody at the pantry gave you a medical kit")
+print("The lady also gives you a splint")
+print("")
+user6 = input("Do you want to go back to headquarters?")
+if user6 == ("No"):
+  print("You have to! We are sorry. You must complete the main quest first. After that it is more open. You start to head back to headquarters.")
+if user6 == ("Yes"):
+  print("You start to head back to headquarters")
+print("Once you are half way back to headquarters you notice how amazing your glasses were. They could call, text, and do a lot more.")
+print("When you were almost at headquarters you notice your leg started feeling better.")
+print("It must've been a minor injury.")
+print("")
+print("When you get to headquarters you see that the fighting has died down to minor fist fights. ")
+print("You see an area where there is people getting healed.")
+user7 = input("Do you want to go there?")
+if user7 == ("Yes"):
+  print("You go over there.")
+if user7 == ("No"):
+  print("I am sorry again but the main level is almost over. You go over to the healing area.")
+print("A docter-like person is laying you down. They give you a numbing shot, then start wrapping your leg with something. But you were to sleepy to notice the pain. You fell asleep...")
+print("")
+time.sleep(2.6)
+print("Wait for the next update for more!")
+print(name)
+print("Till next time.")
